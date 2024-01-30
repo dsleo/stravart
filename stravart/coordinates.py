a8168c6e24d64bb5e7db9b6ae84af8881f166933
dsleo <leooleds@gmail.com>
2024-01-30 16:51:24 +0100

    mapbox instead of overpass

--- stravart/coordinates.py
+++ stravart/coordinates.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import List
 import numpy as np
 import requests
+from config import MAPBOX_ACCESS_TOKEN
 from geopy.distance import great_circle
 
 @dataclass(frozen=True)
@@ -63,6 +64,41 @@ class Coordinates:
 
     def get_nearest_bicycle_road_point(self, dist=1000):
         try:
+            mode = "cycling"
+            access_token = MAPBOX_ACCESS_TOKEN
+            coordinates = f"{self.longitude},{self.latitude};{self.longitude},{self.latitude}"
+            url = f"https://api.mapbox.com/directions/v5/mapbox/{mode}/{coordinates}"
+
+            params = {
+                'access_token': access_token,
+                'geometries': 'geojson',
+                'radiuses': f"{dist};{dist}"
+            }
+
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            if data['code'] != 'Ok':
+                raise Exception("Mapbox API error: " + data['code'])
+
+            nearest_point = None
+            min_distance = float('inf')
+            for route in data['routes']:
+                for leg in route['legs']:
+                    for step in leg['steps']:
+                        step_point = (step['maneuver']['location'][1], step['maneuver']['location'][0])
+                        distance = great_circle((self.latitude, self.longitude), step_point).meters
+                        if distance < min_distance:
+                            nearest_point = step_point
+                            min_distance = distance
+
+            return nearest_point if nearest_point else None
+
+        except Exception as e:
+            print(f"Error getting closest bicycle coordinate for {(self.latitude, self.longitude)}: {e}")
+            return None
+'''
+        try:
             overpass_url = "http://overpass-api.de/api/interpreter"
             lat, lon  = self.latitude, self.longitude
             overpass_query = f"""
@@ -110,3 +146,4 @@ class Coordinates:
         except Exception as e:
             print(f"Error getting closest bicycle coordinate for {(lat, lon)}: {e}")
             return None
+'''
